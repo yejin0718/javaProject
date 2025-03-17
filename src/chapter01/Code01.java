ff3773b1f42a9eb0d1da721105ae12990eb02256
kimyejin <a89557400@gmail.com>
2025-03-17 11:52:32 +0900

    main Method 사용법, 연결연산자, 덧셈연산자, println, print차이점

---
+++ src/chapter01/Code01.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+public class Code01 {
+    public static void main(String[] args) {
+        System.out.println("AI Software");
+        System.out.print("김예진\n");
+        System.out.printf("Grade 1\n");
+        System.out.println("result:" + 100 + 200);
+        System.out.println("result:"+(100 + 200));
+        System.out.println(100 + 200 + "won");
+    }
+}
---
+++ src/chapter01/Code02_07.java
@@ -0,0 +1,20 @@
+package chapter01;
+
+public class Code02_07 {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 200;
+
+        int result = num1 + num2;
+        System.out.printf("%d + %d = %d\n", num1, num2, result);
+
+        result = num1 - num2;
+        System.out.printf("%d - %d = %d\n", num1, num2, result);
+
+        result = num1 * num2;
+        System.out.printf("%d * %d = %d\n", num1, num2, result);
+
+        result = num1 / num2;
+        System.out.printf("%d / %d = %d\n", num1, num2, result);
+    }
+}
