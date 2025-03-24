69eaf5527132e7454e7165bfd8ba61e279ce916a
kimyejin <a89557400@gmail.com>
2025-03-24 11:35:02 +0900

    정수/정수 => 정수, 정수/실수 또는 실수/정수 또는 실수/실수 => 실수

---
+++ src/chapter01/Code02_09.java
@@ -0,0 +1,16 @@
+package chapter01;
+
+public class Code02_09 {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        double result;
+
+        result = num1 + num2;
+        System.out.printf("%d + %d = %.1f\n", num1, num2, result);
+
+//      정수형 / 정수형 => 정수구만 저장
+//      정수형 둘 중에 하나를 실수형으로 변환해서 연산
+        result = num1 / (double)num2;
+        System.out.printf("%d / %d = %.1f", num1, num2, result);
+    }
+}
