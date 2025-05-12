26b0111e11aadf2afea2a8c9f69a451edd8a5cce
yejin0718 <a89557400@gmail.com>
2025-05-12 10:34:49 +0900

    6장 반복문-for4, 홀수의 합계로 수정

--- src/chapter06/ForTest4.java
+++ src/chapter06/ForTest4.java
@@ -3,11 +3,15 @@ package chapter06;
 public class ForTest4 {
     public static void main(String[] args) {
         int hap = 0;
+        String oddEven = "";
 
         for(int i=1; i<=1000; i++) {
-            hap += i;
+            if(i % 2 == 0) {
+                hap += i;
+                oddEven = "짝수";
+            }
         }
 
-        System.out.println(hap);
+        System.out.println("1-100 까지의 " + oddEven + "의 합계는 " + hap + "이다.");
     }
 }
