58aea60f3d4518e21fef6449d42680cb49d964d1
yejin0718 <a89557400@gmail.com>
2025-05-12 10:51:30 +0900

    6장 반복문-for6, 중첩된 for문

---
+++ src/chapter06/ForTest6.java
@@ -0,0 +1,11 @@
+package chapter06;
+
+public class ForTest6 {
+    public static void main(String[] args) {
+        for (int i=0; i<3; i++) {
+            for (int j=0; j< 2; j++) {
+                System.out.printf("인공지능소프트웨어 알고리즘실습 수업: (i:,%d j:%d)\n",i,j);
+            }
+        }
+    }
+}
