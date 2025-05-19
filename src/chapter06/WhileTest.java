1adfea18a01d0f26b289deda2420ba496d57d81b
yejin0718 <a89557400@gmail.com>
2025-05-19 09:21:40 +0900

    6장 반복문-while문 for문과 같이 횟수를 정해놓고 사용하는 경우

---
+++ src/chapter06/WhileTest.java
@@ -0,0 +1,11 @@
+package chapter06;
+
+public class WhileTest {
+    public static void main(String[] args) {
+        int i=0;
+        while(i < 5) {
+            System.out.println(i + 1 +"번: 인공지능소프트웨어과");
+            i++;
+        }
+    }
+}
