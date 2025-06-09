bbb9aac4b708d6c3e3f7cb1597d674db68fcd65e
yejin0718 <a89557400@gmail.com>
2025-06-09 11:44:09 +0900

    7장 클래스와 객체 생성자 LAB02 주사위 실습

---
+++ src/chapter07/dice/Dice.java
@@ -0,0 +1,12 @@
+package chapter07.dice;
+
+public class Dice {
+    private int number;
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public int getNumber() {
+        return this.number;
+    }
+}
---
+++ src/chapter07/dice/DiceTest.java
@@ -0,0 +1,24 @@
+package chapter07.dice;
+
+public class DiceTest {
+    public static void main(String[] args) {
+        int count = 0;
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+        Dice dice3 = new Dice();
+
+        while (true) {
+            count++;
+            dice1.setNumber((int) (Math.random()*6 + 1));
+            dice2.setNumber((int) (Math.random()*6 + 1));
+            dice3.setNumber((int) (Math.random()*6 + 1));
+
+            if (dice1.getNumber() == dice2.getNumber()
+            && dice2.getNumber() == dice3.getNumber()) {
+                break;
+            }
+        }
+        System.out.printf("3개 주사위는 모두 %d입니다.\n", dice1.getNumber());
+        System.out.printf("같은 숫자가 나올 때까지 %d번 던졌습니다.\n", count);
+    }
+}
